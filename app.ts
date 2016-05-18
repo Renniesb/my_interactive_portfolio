b325631b4414ecc456cb9199923624a19b8d149f
Rennie Bevineau <rsbevineau@gmail.com>
2016-05-18 16:41:55 -0600

    add technologies used to html/css

--- app.ts
+++ app.ts
@@ -4,11 +4,13 @@ import { Component } from 'angular2/core';
 class Project {
   title: string;
   link: string;
+  technologies: Array<string>;
   votes: number;
 
-  constructor(title: string, link: string, votes?: number) {
+  constructor(title: string, link: string, technologies: Array<string> , votes?: number) {
     this.title = title;
     this.link = link;
+    this.technologies = technologies;
     this.votes = votes || 0;
   }
 
@@ -29,6 +31,10 @@ class Project {
 					<a href="{{project.link}}" class="btn btn-primary">See live site</a>
 				  </div>
 			</div>
+			<div class="col-sm-6">
+				<h1>Technology used</h1>
+				<p>{{project.technologies}}</p>
+			</div>
 		</div>
 	</div>
 	`
@@ -38,8 +44,8 @@ class ProjectApp {
 	projects: Project[];
 	constructor(){
 		this.projects = [
-			new Project('Project 1','www.renniewebcreations.com'),
-			new Project('Project 2','www.google.com')
+			new Project('Project 1','www.renniewebcreations.com', ['angular', 'html', 'css']),
+			new Project('Project 1','www.renniewebcreations.com', ['bootstrap', 'html', 'css']),
 		];
 	}
 }
